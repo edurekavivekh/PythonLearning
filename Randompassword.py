0d034cd875998bd263503250df21c0defcbcaf2f
edurekavivekh <vivek.hebalkar@gmail.com>
2022-03-03 15:17:19 +0000

    naming convention changed

--- Randompassword.py
+++ Randompassword.py
@@ -4,35 +4,44 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n")) 
+print("Welcome to the Pydata Generator!")
+nr_letters= int(input("How many letters would you like in your data?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-letters_selected = []
-symbols_selected = []
-numbers_selected = []
+letters_pickup = []
+symbols_pickup = []
+numbers_pickup = []
+
+#Pickup random letters from letters list
 for i in range(0,int(nr_letters)):
-  random_letter = random.randint(0,len(letters) - 1)
-  letters_selected.append(letters[random_letter])
+  rnd_letter_index = random.randint(0,len(letters) - 1)
+  letters_pickup.append(letters[rnd_letter_index])
+
+#Pickup random symbols from symbols list
 for j in range(0,int(nr_symbols)):
-  random_symbols = random.randint(0,len(symbols) - 1)
-  symbols_selected.append(symbols[random_symbols])
+  rnd_symbols_index = random.randint(0,len(symbols) - 1)
+  symbols_pickup.append(symbols[rnd_symbols_index])
+
+#Pickup random numbers from numbers list
 for k in range(0,int(nr_numbers)):
-  random_numbers = random.randint(0,len(numbers) - 1)
-  numbers_selected.append(numbers[random_numbers])
-password = []
-for l in letters_selected:
-  password.append(l)
-for m in symbols_selected:
-  password.append(m)
-for n in numbers_selected:
-  password.append(n)
+  rnd_numbers_index = random.randint(0,len(numbers) - 1)
+  numbers_pickup.append(numbers[rnd_numbers_index])
 
-random.shuffle(password)
-items = ""
-for i in password:
-    items = items + i
-print(f'Your password is: {items}')
+data = []
+
+#Create combined list of numbers, symbols and letters
+for l in letters_pickup:
+  data.append(l)
+for m in symbols_pickup:
+  data.append(m)
+for n in numbers_pickup:
+  data.append(n)
 
 
+random.shuffle(data)
+password = ""
+for i in data:
+    password = password + i
+
+print(f'Your password is: {password}')
